e1ca1a68e1d85f18d372bc39e3f14e1285668694
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-01-26 17:25:27 +0100

    remove broken link

--- packages/app/components/GnosisLogo.tsx
+++ packages/app/components/GnosisLogo.tsx
@@ -1,17 +1,16 @@
 import Image from "next/image"
+import Link from "next/link"
 import cls from "./GnosisLogo.module.css"
 
 const GnosisLogo: React.FC = () => (
   <div className={cls.container}>
-    <a href="/">
-      <Image 
-        className={cls.logo}
-        src="/gno.svg"
-        alt="Gnosis Logo"
-        height={36}
-        width={36}
-      />
-    </a>
+    <Image
+      className={cls.logo}
+      src="/gno.svg"
+      alt="Gnosis Logo"
+      height={36}
+      width={36}
+    />
   </div>
 )
 
