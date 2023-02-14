ce949396c2a736072af5e984c4bcae81435c6294
samepant <sampanter@gmail.com>
2023-02-14 15:28:33 -0500

    adds use gno banner

---
+++ packages/app/components/UseGnoBanner/index.tsx
@@ -0,0 +1,20 @@
+import Card from "../Card"
+
+import classes from "./UseGnoBanner.module.css"
+
+const UseGNOBanner = () => {
+  return (
+    <a href="https://www.usegno.com/" className={classes.cardLink}>
+      <Card className={classes.card}>
+        <img src="/gnochainfuture.png" alt="an imagined solarpunk future" />
+        <h2>Not sure where to use your GNO?</h2>
+        <p>
+          Click here to explore all the valuable uses on Gnosis Chain and
+          Ethereum
+        </p>
+      </Card>
+    </a>
+  )
+}
+
+export default UseGNOBanner
